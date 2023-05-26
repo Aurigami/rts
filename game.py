3295e3dcfc4df9ba03c48b44db9d15b20c197525
Aurigami <aurigamifr@gmail.com>
2023-05-27 00:37:15 +0200

    split off TargetCursor

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import math
 from constants import *
 from Unit import Unit
 from SelectionRect import SelectionRect
+from TargetCursor import TargetCursor
 
 # Set up Pygame
 pygame.init()
@@ -11,30 +12,6 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("RTS Game")
 clock = pygame.time.Clock()
 FPS = 60
-
-class TargetCursor(pygame.sprite.Sprite):
-    # TODO: cursor stops when any unit stop (shouldn't be that way)
-    # TODO: one cursor per unit
-    # TODO: Cursor shows up ONLY when the unit is selected (to see where it goes)
-    def __init__(self):
-        super().__init__()
-        self.size = (UNIT_SIZE[0] // 1.3, UNIT_SIZE[0] // 1.3)
-        self.pos = (0, 0)
-        self.image = pygame.Surface(self.size, pygame.SRCALPHA)
-        self.rect = self.image.get_rect()
-        self.rect.center = (0, 0)
-        self.radius = UNIT_SIZE[0] // 2.6
-        # self.visible = False
-        self.target = None
-
-    def show(self):
-        self.rect.center = self.target
-        pygame.draw.circle(self.image, TARGET_CURSOR_MOVE_COLOR, (self.radius, self.radius), self.radius)
-        # target_cursor.visible = True
-
-    # def hide(self):
-    #     self.image.fill(TRANSPARENT)
-    #     target_cursor.visible = False
 
 # Set up game objects
 all_sprites = pygame.sprite.Group()
@@ -49,7 +26,6 @@ for i in range(50):
     unit = Unit((x, y))
     all_sprites.add(unit)
     units.add(unit)
-
 
 # Define helper functions
 # def get_unit_under_mouse(pos):

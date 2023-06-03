84f63cee2eda9a3fa96e878578d02222ea70adca
Aurigami <aurigamifr@gmail.com>
2023-06-03 17:44:38 +0200

    add graphics menu

--- Menu.py
+++ Menu.py
@@ -13,6 +13,10 @@ def set_difficulty(value, difficulty):
     # Do the job here !
     pass
 
+def set_graphics(value, new_window_size):
+    # todo
+    pass
+
 def start_the_game():
     pause_menu.enable()
     # Do the job here !
@@ -24,10 +28,11 @@ def resume():
 def disabled():
     pause_menu.disable()
 
-pause_menu = pygame_menu.Menu('Menu', WINDOW_SIZE[0], WINDOW_SIZE[1],
-                       theme=pygame_menu.themes.THEME_BLUE)
-options_menu = pygame_menu.Menu('Options', WINDOW_SIZE[0], WINDOW_SIZE[1],
-                      theme=pygame_menu.themes.THEME_DARK)
+pause_menu = pygame_menu.Menu('Menu', WINDOW_SIZE[0], WINDOW_SIZE[1], theme=pygame_menu.themes.THEME_DARK)
+
+options_menu = pygame_menu.Menu('Options', WINDOW_SIZE[0], WINDOW_SIZE[1], theme=pygame_menu.themes.THEME_DARK)
+
+graphical_options_menu = pygame_menu.Menu('Options', WINDOW_SIZE[0], WINDOW_SIZE[1], theme=pygame_menu.themes.THEME_DARK)
 
 # menu.add.text_input('Name :', default='John Doe')
 # menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
@@ -35,4 +40,8 @@ pause_menu.add.button('Play', resume)
 pause_menu.add.button('Options', options_menu)
 pause_menu.add.button('Quit', pygame_menu.events.EXIT)
 
+options_menu.add.button('Graphics', graphical_options_menu)
 options_menu.add.button('Return', pygame_menu.events.BACK)
+
+graphical_options_menu.add.selector('Resolution', [('1920x1080', (1920,1080)), ('1280x720', (1280, 720)), ('800x600', (800, 600))], onchange=set_graphics)
+graphical_options_menu.add.button('Return', pygame_menu.events.BACK)
